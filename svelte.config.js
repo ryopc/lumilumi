b8bca6b15ff1075687385cf5becb3801df477f1c
game_ryo <ryotagtagtag@gmail.com>
2026-05-26 15:32:35 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,20 @@
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-cloudflare"; // 👈 cloudflareに変更
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter(), // 👈 cloudflareアダプターを有効化
     serviceWorker: {
-      //pwa仮想モジュール（virtual:pwa-registerまたはvirtual:pwa-register/svelte）を使用している場合は、SvelteKitの設定からサービスワーカーの登録を除外する必要があります：
+      // PWAプラグイン側で制御するため、SvelteKit標準の登録はオフ
       register: false,
     },
-
-    // 旧:csrf: { checkOrigin: false },
-    // これつけないと共有のときにCross-site POST form submissions are forbiddenがでる
-
     csrf: {
       trustedOrigins: ["*"],
     },
   },
-  // compilerOptions: { runes: true }
 };
+
 export default config;
